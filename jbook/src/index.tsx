3e63fde9a5102e7e8f6f06431a292bcd57718cca
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2021-02-24 13:07:50 +0200

    Displaying a Code Editor In a React App

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,8 +1,11 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import MonacoEditor from './components/code-editor';
+import CodeEditor from './components/code-editor';
 
 const App = () => {
   const ref = useRef<any>();
@@ -61,6 +64,8 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor initialValue="const a = 1;" 
+      onChange={(value) => setInput(value)}/>
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
@@ -68,7 +73,12 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+      <iframe
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
     </div>
   );
 };
