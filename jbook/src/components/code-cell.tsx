7baabb4e92520ca40853eb362f11768e69955d00
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2021-02-26 18:23:17 +0200

    Adding a 'Cumulative Code Execution' Feature
    Executing the Cumulative Code.
    Adding a Show Function.
    Showing Complex Values.
    Showing a JSX Element.
    Import Name Collisions.
    Avoiding Naming Collisions.
    Selective Calls to Show.
    Implementing a Show Function No-Op
    A Touch of CSS.
    Fixing Edge Snaps.

--- jbook/src/components/code-cell.tsx
+++ jbook/src/components/code-cell.tsx
@@ -6,6 +6,7 @@ import { Cell } from '../state';
 import { useActions } from '../hooks/use-actions';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import './code-cell.css';
+import { useCumulativeCode } from '../hooks/use-cumulative-code';
 
 interface CodeCellProps {
   cell: Cell;
@@ -14,35 +15,23 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
-  const cumulativeCode = useTypedSelector((state) => {
-    const { data, order } = state.cells;
-    const orderedCell = order.map((id) => data[id]);
-    const cumulativeCode = [];
-    for (let c of orderedCell) {
-      if (c.type === 'code') {
-        cumulativeCode.push(c.content);
-      }
-      if (c.id === cell.id) {
-        break;
-      }
-    }
-  });
+  const cumulativeCode = useCumulativeCode(cell.id);
 
   useEffect(() => {
     if (!bundle) {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode);
       return;
     }
 
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode);
     }, 750);
 
     return () => {
       clearTimeout(timer);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cell.content, cell.id, createBundle]);
+  }, [cumulativeCode, cell.id, createBundle]);
 
   return (
     <Resizable direction="vertical">
