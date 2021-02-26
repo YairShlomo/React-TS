827ce6395ef73dc0dc6dbad4358b5b38cc22eba5
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2021-02-26 21:32:16 +0200

    Creating the CLI
    Using Commander to Build a CLI.
    Specifying Command Line Options.
    Communication from the CLI to the API.
    Calculating File Paths.

--- jbook/packages/cli/src/index.ts
+++ jbook/packages/cli/src/index.ts
@@ -1,3 +1,9 @@
-import serve from 'local-api';
+// import serve from 'local-api';
+// serve();
+import { program } from 'commander';
+import { serveCommand } from './commands/serve';
 
-serve();
+program
+.addCommand(serveCommand);
+
+program.parse(process.argv)
